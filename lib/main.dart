4dbd51ce569ecce45c9234fd222b315b292a070b
michael <michaelaina8@gmail.com>
2024-12-30 05:10:38 +0100

    chores

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,38 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:provider/provider.dart';
+import 'package:yum_quick/models/user.dart';
+import 'package:yum_quick/routes.dart';
+import 'package:yum_quick/views/auth/auth.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();  // Ensures binding before initialization
+
+  // Initialize Firebase before running the app
+  await Firebase.initializeApp();  // This needs to be awaited
+
   runApp(const MyApp());
 }
-
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return ScreenUtilInit(
+      designSize: const Size(360, 690), // Adjust for your design
+      minTextAdapt: true,
+      builder: (context, child) {
+        return StreamProvider<CustomUser?>.value(
+          value: AuthService().user,  // This should be a stream of CustomUser?
+          initialData: null,
+          child: MaterialApp(
+            debugShowCheckedModeBanner: false,
+            initialRoute: AppRoutes.splash,
+            routes: AppRoutes.getRoutes(),
+          ),
+        );
+      },
     );
   }
 }
---
+++ lib/routes.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/cupertino.dart';
+import 'package:yum_quick/core/constants/home.dart';
+import 'package:yum_quick/views/menu%20slide/contact%20us/contact_us.dart';
+import 'package:yum_quick/views/menu%20slide/delivery%20adress/delivery_address.dart';
+import 'package:yum_quick/views/menu%20slide/delivery%20adress/new_address.dart';
+import 'package:yum_quick/views/menu%20slide/my%20profile/my_profile.dart';
+import 'package:yum_quick/views/menu%20slide/orders/my_orders.dart';
+import 'package:yum_quick/views/menu%20slide/payment/add_card.dart';
+import 'package:yum_quick/views/menu%20slide/payment/payment_method.dart';
+import 'package:yum_quick/views/screens/forget_password.dart';
+import 'package:yum_quick/views/screens/home/home_screen.dart';
+import 'package:yum_quick/views/screens/onboarding/onboarding1.dart';
+import 'package:yum_quick/views/screens/onboarding/onboarding2.dart';
+import 'package:yum_quick/views/screens/onboarding/onboarding3.dart';
+import 'package:yum_quick/views/screens/register.dart';
+import 'package:yum_quick/views/screens/second_splash.dart';
+import 'package:yum_quick/views/screens/sign_in.dart';
+import 'package:yum_quick/views/screens/splash.dart';
+
+class AppRoutes {
+  static const String splash = '/';
+  static const String secondSplash = '/second-splash';
+  static const String onBoarding1 = "/onboarding1";
+  static const String onBoarding2 = "/onboarding2";
+  static const String onBoarding3 = "/onboarding3";
+  static const String login = "/login_page";
+  static const String register = "/register";
+  static const String home = "/home";      // Added Home Route
+  static const String homeScreen = "/home_screen";
+  static const String forgetPassword = "/forget_passwrod";
+  static const String myOrders = "/my_orders";
+  static const String myProfile = "/my_profile";
+  static const String deliveryAddress = "/delivery_address";
+  static const String newAddress = "/new_address";
+  static const String paymentMethod = "/payment_method";
+  static const String addCard = "/add_card";
+  static const String contactUs = "/contact_us";
+
+  static Map<String, WidgetBuilder> getRoutes() {
+    return {
+      splash: (context) => const Splash(),
+      secondSplash: (context) => const SecondSplash(),
+      onBoarding1: (context) => const Onboarding1(),
+      onBoarding2: (context) => const Onboarding2(),
+      onBoarding3: (context) => const Onboarding3(),
+      login: (context) => const LoginPage(),
+      register: (context) => const Register(),
+      home: (context) => const Home(),    // Linked to Home Widget
+      homeScreen: (context) => const HomeScreen(),
+      forgetPassword: (context) => const ForgetPassword(),
+      myOrders: (context) => const MyOrders(),
+      myProfile: (context) => const MyProfile(),
+      deliveryAddress: (context) => const DeliveryAddress(),
+      newAddress: (context) => const NewAddress(),
+      paymentMethod: (context) => const PaymentMethod(),
+      addCard: (context) => const AddCard(),
+      contactUs: (context) => const ContactUs(),
+    };
+  }
+}
